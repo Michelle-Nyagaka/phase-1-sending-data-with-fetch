c272f0f414d18bd7c131cb5459f3897b3aba9a5c
Michelle Nyagaka <michellenyagaka@gmail.com>
2025-09-25 20:46:51 +0300

    my js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // append ID to DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = data.id;
+      body.appendChild(p);
+    })
+    .catch((error) => {
+      const body = document.querySelector("body");
+      const errorMessage = document.createElement("p");
+      errorMessage.textContent = error.message;
+      body.appendChild(errorMessage);
+    });
+}
